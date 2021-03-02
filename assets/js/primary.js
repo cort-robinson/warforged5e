3bf68571db776658789c154105108f969328e199
cort-robinson <cortrobinson99@gmail.com>
2021-03-02 18:14:54 +0000

    add searchbar functionality

--- assets/js/primary.js
+++ assets/js/primary.js
@@ -1,8 +1,38 @@
-$(document).ready(function () {
-  $.get('https://www.dnd5eapi.co/api/monsters/', function (data) {
-    const monsters = data.results;
-    for (monster of monsters) {
-      $('.monsterList').append('<li>' + monster.name + '</li>')
-    }
-  });
+const monstersList = document.getElementById('monstersList');
+const searchBar = document.getElementById('searchBar');
+let monsters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
+  const filteredMonsters = monsters.filter((monster) => {
+    return (
+      monster.name.toLowerCase().includes(searchString)
+    );
+  })
+  displayMonsters(filteredMonsters);
 });
+
+const loadMonsters = async () => {
+  try {
+    const res = await $.get('https://www.dnd5eapi.co/api/monsters/');
+    monsters = await res.results;
+    displayMonsters(monsters);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const displayMonsters = (monsters) => {
+  const htmlString = monsters
+    .map((monster) => {
+      return `
+      <li class="monster">
+          <h2>${monster.name}</h2>
+      </li>
+    `;
+    })
+    .join('');
+  monstersList.innerHTML = htmlString;
+}
+
+loadMonsters();
