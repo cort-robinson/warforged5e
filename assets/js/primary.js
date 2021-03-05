3baadf7c5dc387e71770ab8c114d902e14d15129
cort-robinson <cortrobinson99@gmail.com>
2021-03-04 20:31:28 -0600

    update with skeleton framework

--- assets/js/primary.js
+++ assets/js/primary.js
@@ -1,38 +1,57 @@
-const monstersList = document.getElementById('monstersList');
-const searchBar = document.getElementById('searchBar');
-let monsters = [];
+/*
+primary.js - main js file
+*/
+$(document).ready(function () {
+    const searchBar = document.getElementById('searchBar');
+    let monsters = [];
+    let monstersObjs = [];
+  
+    searchBar.addEventListener('keyup', (e) => {
+      // Search bar function - listens to key strokes
+      const searchString = e.target.value.toLowerCase();
+      const filteredMonsters = monsters.filter((monster) => {
+        return monster.name.toLowerCase().includes(searchString);
+      });
+      displayMonsters(filteredMonsters);
+    });
+  
+    const loadMonsters = async () => {
+      // Load the monsters API list and calls displayMonsters
+      try {
+        const res = await $.get('https://www.dnd5eapi.co/api/monsters/');
+        monsters = await res.results;
+        displayMonsters(monsters);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+    const displayMonsters = (monsters) => {
+      // Takes list from loadMonsters and outputs that into the html
+      const htmlString = monsters
+      .map((monster) => {
+        return `
+        <li class="monster"><button class="addMonster" id=${monster.index}>Add</button>&nbsp;${monster.name}</li>`;
+      })
+      .join('');
+      $('#monstersList').html(htmlString);
+    };
+  
+    loadMonsters();
+    // Call loadMonsters to initiate
+  
+    $('#monstersList').on('click', '.addMonster', () => {
+      $.when(
+        $.getJSON('https://www.dnd5eapi.co/api/monsters/' + $(this.activeElement).attr('id'))
+    ).done( function(json) {
+        monstersObjs.push(json);
+        // temporary command for testing purposes
+        console.log(monstersObjs);
+    });
+    });
 
-searchBar.addEventListener('keyup', (e) => {
-  const searchString = e.target.value.toLowerCase();
-  const filteredMonsters = monsters.filter((monster) => {
-    return (
-      monster.name.toLowerCase().includes(searchString)
-    );
-  })
-  displayMonsters(filteredMonsters);
-});
-
-const loadMonsters = async () => {
-  try {
-    const res = await $.get('https://www.dnd5eapi.co/api/monsters/');
-    monsters = await res.results;
-    displayMonsters(monsters);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-const displayMonsters = (monsters) => {
-  const htmlString = monsters
-    .map((monster) => {
-      return `
-      <li class="monster">
-          <h2>${monster.name}</h2>
-      </li>
-    `;
-    })
-    .join('');
-  monstersList.innerHTML = htmlString;
-}
-
-loadMonsters();
+    $('#searchBar').focus( () => {
+        $('#monstersList').removeClass('inactive');
+    }) 
+  });
+  
