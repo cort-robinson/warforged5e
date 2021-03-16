b677a48db94bfb4ece97ac5ecfb0230a9e497ca4
cort-robinson <cortrobinson99@gmail.com>
2021-03-16 12:06:32 -0500

    Update primary.js

--- assets/js/primary.js
+++ assets/js/primary.js
@@ -74,7 +74,7 @@ $(document).ready(function () {
 
   loadMonsters(); // Call loadMonsters to initiate
 
-  $('#monstersList').on('click', '.addMonster', () => {
+  $('#monstersList').on('touchstart click', '.addMonster', () => {
     $.when(
       $.getJSON(
         'https://www.dnd5eapi.co/api/monsters/' +
