05f94e4d2c578a7c62c9fffc5c759b2c2f907deb
cort-robinson <cortrobinson99@gmail.com>
2021-03-16 14:49:11 -0500

    update dropdown

--- assets/js/primary.js
+++ assets/js/primary.js
@@ -157,4 +157,17 @@ $(document).ready(function () {
       displaySelected(monstersObjs);
     });
   });
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      let dropdowns = document.getElementsByClassName('dropdown-content')
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 });
