4eaf628fc1156eb5bf86ccb74c94d78d62ce450f
cort-robinson <cortrobinson99@gmail.com>
2021-03-16 14:50:33 -0500

    revert previous changes

--- assets/js/primary.js
+++ assets/js/primary.js
@@ -157,17 +157,4 @@ $(document).ready(function () {
       displaySelected(monstersObjs);
     });
   });
-
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      let dropdowns = document.getElementsByClassName('dropdown-content')
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
 });
